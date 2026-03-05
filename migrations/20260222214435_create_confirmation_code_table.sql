2bc542d91b0ec7279e864c3cab8326f9b669ce69
Joao Otavio Cano <joaootaviocano@MacBook-Air-de-Joao.local>
2026-03-05 20:39:38 -0300

    fix: syntax

--- migrations/20260222214435_create_confirmation_code_table.sql
+++ migrations/20260222214435_create_confirmation_code_table.sql
@@ -5,14 +5,13 @@ CREATE TABLE confirmation_codes (
 	code CHAR(7) NOT NULL,
 	expireAt TIMESTAMP WITH TIME ZONE NOT NULL,
 	userID UUID NOT NULL,
-	digitalAddr NOT NULL
-	codeType code_type NOT NULL
-	data VARCHAR(50)
+	digitalAddr VARCHAR(50) NOT NULL,
+	codeType code_type NOT NULL,
+	data VARCHAR(50),
 
-
-    FOREIGN KEY (userID) REFERENCES users (id)
+    FOREIGN KEY (userID) REFERENCES users (id),
     PRIMARY KEY (code, digitalAddr)
-)
+);
 
 -- +goose Down
-DROP TABLE confirmation_codes
+DROP TABLE confirmation_codes;
